f053c43b5cd1c868bc4c0c963be7b607453f41be
朱赞聪 <1147550511@qq.com>
2018-09-16 18:48:56 +0800

    判断用户名是否存在

--- front_end_pc/js/register.js
+++ front_end_pc/js/register.js
@@ -24,6 +24,7 @@ var vm = new Vue({
         allow: false
     },
     methods: {
+        // 检查用户名
         check_username: function () {
             var len = this.username.length;
             if (len < 5 || len > 20) {
@@ -32,7 +33,23 @@ var vm = new Vue({
             } else {
                 this.error_name = false;
             }
-
+             // 检查重名
+            if (this.error_name == false) {
+                axios.get(this.host + '/usernames/' + this.username + '/count/', {
+                        responseType: 'json'
+                    })
+                    .then(response => {
+                        if (response.data.count > 0) {
+                            this.error_name_message = '用户名已存在';
+                            this.error_name = true;
+                        } else {
+                            this.error_name = false;
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error.response.data);
+                    })
+            }
         },
         check_pwd: function () {
             var len = this.password.length;
--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from users.models import User
+
+
+class UsernameCountView(APIView):
+    """
+    判断用户名是否存在
+    """
+
+    def get(self, request, username):
+        # 根据username在数据库查询username的数量
+        count = User.objects.filter(username=username).count()
+
+        data = {
+            "username": username,
+            "count": count
+        }
+        # 将数据返回
+        return Response(data)
